e0d7db9489a770db0c808eb4ad2673a57215c6c5
Esteban Basili <esteban.ba@gmail.com>
2022-05-06 14:34:44 +0200

    Iteration 01 Done

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,21 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    console.log(`The soldier/'s previous health was: ${this.health}.`)
+    this.health = this.health - damage;
+    console.log(`The soldier/'s current health is: ${this.health}.`)
+  }
+}
+
+const newSoldier = new Soldier (100, 100);
+newSoldier.receiveDamage(20);
 
 // Viking
 class Viking {}
